8069ddb65f250eebd152a1b4d0461c1636157fb3
UmranRahman <umranrahman0@gmail.com>
2026-06-18 01:28:12 -0600

    fixed tests

--- tests/test_sphere.cpp
+++ tests/test_sphere.cpp
@@ -10,17 +10,17 @@ static bool approxEqual(float a, float b, float eps = 1e-5f) {
 // optionally add Vec3 approxEqual if needed
 
 int main() {
-    Sphere sphere(Vec3{0.0f, 0.0f, -5.0f}, 1.0f);
+    Sphere sphere(Vec3{0.0f, 0.0f, -5.0f}, 1.0f, Vec3{1.0f, 1.0f, 1.0f});
 
     Ray ray(Vec3{0.0f, 0.0f, 0.0f}, Vec3{0.0f, 0.0f, -1.0f});
-    float t;
-    bool hit = sphere.hit(ray, 0.001f, 1000.0f, t);
+    HitRecord rec;
+    bool hit = sphere.hit(ray, 0.001f, 1000.0f, rec);
 
     assert(hit);
-    assert(approxEqual(t, 4.0f)); // or just assert(t > 0.0f)
+    assert(approxEqual(rec.t, 4.0f)); // or just assert(rec.t > 0.0f)
 
     Ray missRay(Vec3{0.0f, 0.0f, 0.0f}, Vec3{0.0f, 1.0f, 0.0f});
-    assert(!sphere.hit(missRay, 0.001f, 1000.0f, t));
+    assert(!sphere.hit(missRay, 0.001f, 1000.0f, rec));
 
     return 0;
 }
