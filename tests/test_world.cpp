2c1f9ddfea3f5de323f2b6053759d6f3854911bb
UmranRahman <umranrahman0@gmail.com>
2026-06-18 01:30:21 -0600

    fixed tests

--- tests/test_world.cpp
+++ tests/test_world.cpp
@@ -6,19 +6,19 @@
 
 int main() {
     World world;
-    Sphere sphere1(Vec3{0.0f, 0.0f, -5.0f}, 1.0f);
-    Sphere sphere2(Vec3{0.0f, 0.0f, -10.0f}, 1.0f);
+    Sphere sphere1(Vec3{0.0f, 0.0f, -5.0f}, 1.0f, Vec3{1.0f, 0.0f, 0.0f});
+    Sphere sphere2(Vec3{0.0f, 0.0f, -10.0f}, 1.0f, Vec3{0.0f, 1.0f, 0.0f});
 
     world.add(&sphere1);
     world.add(&sphere2);
 
     Ray r(Vec3{0.0f, 0.0f, 0.0f}, Vec3{0.0f, 0.0f, -1.0f});
-    float t;
-    bool hit = world.hit(r, 0.001f, 1000.0f, t);
+    HitRecord rec;
+    bool hit = world.hit(r, 0.001f, 1000.0f, rec);
 
     assert(hit);
-    assert(t > 0.0f);
-    assert(t < 5.5f); // should hit sphere1 first
+    assert(rec.t > 0.0f);
+    assert(rec.t < 5.5f); // should hit sphere1 first
 
     return 0;
 }
